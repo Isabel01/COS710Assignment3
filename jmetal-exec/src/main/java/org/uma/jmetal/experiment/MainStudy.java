0610e5fe2e4719e9b3227c5b159d64932d91b2b2
Hanrich <Hanrichp@gmail.com>
2016-05-01 16:28:13 +0200

    New Approach

--- jmetal-exec/src/main/java/org/uma/jmetal/experiment/MainStudy.java
+++ jmetal-exec/src/main/java/org/uma/jmetal/experiment/MainStudy.java
@@ -9,6 +9,12 @@ import org.uma.jmetal.operator.impl.mutation.PolynomialMutation;
 import org.uma.jmetal.operator.impl.mutation.SimpleRandomMutation;
 import org.uma.jmetal.problem.Problem;
 import org.uma.jmetal.problem.multiobjective.cec2010.SEF;
+import org.uma.jmetal.problem.multiobjective.dtlz.DTLZ1;
+import org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2;
+import org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2M;
+import org.uma.jmetal.problem.multiobjective.dtlz.DTLZ3;
+import org.uma.jmetal.problem.multiobjective.wfg.WFG6;
+import org.uma.jmetal.problem.multiobjective.wfg.WFG7;
 import org.uma.jmetal.problem.multiobjective.zdt.*;
 import org.uma.jmetal.qualityindicator.impl.*;
 import org.uma.jmetal.qualityindicator.impl.hypervolume.PISAHypervolume;
@@ -24,6 +30,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.uma.jmetal.algorithm.multiobjective.moead.MOEADBuilder.Variant.MOEAD;
 
 /**
@@ -37,13 +44,17 @@ public class MainStudy {
             throw new JMetalException("Missing argument: experiment base directory") ;
         }
         String experimentBaseDirectory = args[0] ;
+        List<Problem<DoubleSolution>> problemList = null;
+        try {
+            problemList = Arrays.<Problem<DoubleSolution>>asList(new DTLZ1(20, 4), new DTLZ2(20, 4), new DTLZ3(20, 4), new WFG6(20, 20, 4),new WFG7(20, 20, 4),new DTLZ2M(20, 4));
+        }catch(Exception ex)
+        {
 
-        List<Problem<DoubleSolution>> problemList = Arrays.<Problem<DoubleSolution>>asList(new ZDT1(), new ZDT2(),
-                new ZDT3(), new ZDT4(), new ZDT6()) ;
+        }
 
         List<TaggedAlgorithm<List<DoubleSolution>>> algorithmList = configureAlgorithmList(problemList, INDEPENDENT_RUNS) ;
 
-        List<String> referenceFrontFileNames = Arrays.asList("ZDT1.tsv", "ZDT2.pf", "ZDT3.pf", "ZDT4.pf", "ZDT6.pf") ;
+        List<String> referenceFrontFileNames = asList("DTLZ1.pf","DTLZ2.pf","DTLZ3.pf","WFG6.pf","WFG7.pf","DTLZ2M.pf") ;
 
         Experiment<DoubleSolution, List<DoubleSolution>> experiment =
                 new ExperimentBuilder<DoubleSolution, List<DoubleSolution>>("COS701 Assignment 3 Study")
@@ -52,9 +63,9 @@ public class MainStudy {
                         .setExperimentBaseDirectory(experimentBaseDirectory)
                         .setOutputParetoFrontFileName("FUN")
                         .setOutputParetoSetFileName("VAR")
-                        .setReferenceFrontDirectory("/pareto_fronts")
+                        .setReferenceFrontDirectory("C:/Users/Hanrich Potgieter/Documents/Experiments/Fronts")
                         .setReferenceFrontFileNames(referenceFrontFileNames)
-                        .setIndicatorList(Arrays.asList(
+                        .setIndicatorList(asList(
                                 new Epsilon<DoubleSolution>(), new Spread<DoubleSolution>(), new GenerationalDistance<DoubleSolution>(),
                                 new PISAHypervolume<DoubleSolution>(),
                                 new InvertedGenerationalDistance<DoubleSolution>(), new InvertedGenerationalDistancePlus<DoubleSolution>()))
@@ -103,13 +114,14 @@ public class MainStudy {
                         .build();
                 algorithms.add(new TaggedAlgorithm<List<DoubleSolution>>(algorithm, "PAES", problemList.get(i), run));
             }
-
+            /*
             for (int i = 0; i < problemList.size(); i++) {
                 Algorithm<List<DoubleSolution>> algorithm = new MOEADBuilder(problemList.get(i),MOEAD)
                         .setMaxEvaluations(25000)
                         .build();
                 algorithms.add(new TaggedAlgorithm<List<DoubleSolution>>(algorithm, "MOEAD", problemList.get(i), run));
             }
+            */
 
         }
         return algorithms ;
